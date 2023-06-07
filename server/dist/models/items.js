24521c29e9c4832862a3bc3cd7c954792ef1b6b5
angusmellor <theangusmellor@gmail.com>
2023-06-07 20:54:36 +0100

    fix: export syntax

--- server/dist/models/items.js
+++ server/dist/models/items.js
@@ -35,19 +35,16 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.itemModel = void 0;
-var db_1 = __importDefault(require("./db"));
+var db_1 = require("./db");
 var addItem = function (item) { return __awaiter(void 0, void 0, void 0, function () {
     var addedItem, e_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, db_1.default.item.create({
+                return [4 /*yield*/, db_1.prisma.item.create({
                         data: item
                     })];
             case 1:
@@ -67,7 +64,7 @@ var getAllItems = function () { return __awaiter(void 0, void 0, void 0, functio
         switch (_a.label) {
             case 0:
                 _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, db_1.default.item.findMany()];
+                return [4 /*yield*/, db_1.prisma.item.findMany()];
             case 1:
                 allItems = _a.sent();
                 return [2 /*return*/, allItems];
